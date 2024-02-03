b2ea064801692544b98caa50fd761fe419627c68
arifzaidpp <arifzaidaiju@gmail.com>
2024-02-03 18:11:20 +0530

    Day-02 basic admin page with add product

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,38 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index',{admin:true})
+  var Products = [{
+    image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1662418953/Croma%20Assets/Communication/Mobiles/Images/230106_nxtpnk.png?tr=w-640",
+    title: "IPHONE 11",
+    category: "Mobile",
+    description: "This is a good mobile"
+  },
+  {
+    image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1662424567/Croma%20Assets/Communication/Mobiles/Images/229922_biq8sa.png?tr=w-640",
+    title: "IPHONE 12",
+    category: "Mobile",
+    description: "This is a good mobile"
+  },
+  {
+    image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1664009521/Croma%20Assets/Communication/Mobiles/Images/243468_0_wevddw.png?tr=w-640",
+    title: "IPHONE 13",
+    category: "Mobile",
+    description: "This is a good mobile"
+  },
+  {
+    image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1662703105/Croma%20Assets/Communication/Mobiles/Images/261963_oqrd6j.png?tr=w-640",
+    title: "IPHONE 14",
+    category: "Mobile",
+    description: "This is a good mobile"
+  }]
+  res.render('admin/view-products',{Products, admin:true})
 });
+router.get('/add-product',function (req,res) {
+  res.render('admin/add-product')
+})
+router.post('/add-product',(req,res)=>{
+  console.log(req.body);
+  console.log(req.files.Image);
+})
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,22 +5,26 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   var Products = [{
     image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1662418953/Croma%20Assets/Communication/Mobiles/Images/230106_nxtpnk.png?tr=w-640",
-    title: "IPHONE 11",
+    title: "IPHONE 1",
+    category: "Mobile",
     description: "This is a good mobile"
   },
   {
     image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1662424567/Croma%20Assets/Communication/Mobiles/Images/229922_biq8sa.png?tr=w-640",
-    title: "IPHONE 12",
+    title: "IPHONE 1",
+    category: "Mobile",
     description: "This is a good mobile"
   },
   {
     image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1664009521/Croma%20Assets/Communication/Mobiles/Images/243468_0_wevddw.png?tr=w-640",
-    title: "IPHONE 13",
+    title: "IPHONE 1",
+    category: "Mobile",
     description: "This is a good mobile"
   },
   {
     image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1662703105/Croma%20Assets/Communication/Mobiles/Images/261963_oqrd6j.png?tr=w-640",
-    title: "IPHONE 14",
+    title: "IPHONE 1",
+    category: "Mobile",
     description: "This is a good mobile"
   }]
   res.render('index', {Products, admin:false});
